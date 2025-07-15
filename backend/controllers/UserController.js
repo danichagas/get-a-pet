3399ed364fda23fdd71bf7ff2e2d2999a182350c
danichagas <chagasdaniel788@gmail.com>
2025-07-15 14:05:26 -0300

    feat: rescuing user by id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -115,4 +115,19 @@ module.exports = class UserController {
 
     res.status(200).send(currentUSer)
   }
+
+  static async getUserById(req, res) {
+    const id = req.params.id
+
+    const user = await User.findById(id).select('-password')
+
+    if(!user) {
+      res.status(422).json({
+        message: 'Usuário não encontrado'
+      })
+      return
+    }
+
+    res.status(200).json({ user })
+  }
 }
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/UserController')
 router.post('/register', UserController.registerNewUser)
 router.post('/login', UserController.userLogin)
 router.get('/checkuser', UserController.checkingUserByToken)
+router.get('/:id', UserController.getUserById)
 
 module.exports = router
