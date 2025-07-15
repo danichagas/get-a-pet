3250b24348d90467dbfe70765c66edb1f97a8423
danichagas <chagasdaniel788@gmail.com>
2025-07-15 17:29:30 -0300

    feat: saving pet in database

---
+++ backend/controllers/PetController.js
@@ -0,0 +1,60 @@
+const Pet = require('../models/Pet')
+
+const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
+
+module.exports = class PetController {
+  static async createPet(req, res) {
+    const { name, age, weight, color } = req.body
+
+    const available = true
+
+    if(!name) {
+      res.status(422).json({ message: 'O nome é obrigatório!' })
+      return
+    }
+
+    if(!age) {
+      res.status(422).json({ message: 'A idade é obrigatória!' })
+      return
+    }
+    if(!weight) {
+      res.status(422).json({ message: 'O peso é obrigatório!' })
+      return
+    }
+    if(!color) {
+      res.status(422).json({ message: 'A cor é obrigatório!' })
+      return
+    }
+
+    const token = getToken(req)
+    const user = await getUserByToken(token)
+
+    const pet = new Pet({
+      name,
+      age,
+      weight,
+      color,
+      available,
+      images: [],
+      user: {
+        _id: user._id,
+        name: user.name,
+        image: user.image,
+        phone: user.phone,
+      },
+    })
+
+    try {
+
+      const newPet = await pet.save()
+      res.status(201).json({
+        message: 'Seu bixinho foi cadastrado com sucesso!',
+        newPet,
+      })
+
+    } catch(err) {
+      res.status(500).json({ message: err })
+    }
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use(express.static('public'))
 
 const UserRoutes = require('./routes/UserRoutes')
+const PetRoutes = require('./routes/PetRoutes')
 
 app.use('/users', UserRoutes)
+app.use('/pets', PetRoutes)
 
 app.listen(5000)
